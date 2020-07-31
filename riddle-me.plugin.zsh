cfa8ce793bdaf693b2a04a4b98bfa90712b57939
venkatakolagotla <venkata.kolagotla@gmail.com>
2020-07-31 15:50:05 +0200

    Add word count hint

--- riddle-me.plugin.zsh
+++ riddle-me.plugin.zsh
@@ -9,29 +9,36 @@ function riddle-me {
     R=${R%'</p></div'*}
     R=${R%' </p></div'*}
     R=${R%'.</p></div'*}
-    R=$(echo "$R" | sed -e 's/<\/br\>.*//g')
-
+    # formatted riddle
+    R=$(echo "$R" | sed -e 's|<br/>||g')
+    # formatted answer
     TXT_A=${TXT_R#*'<p><strong>Answer: </strong>'}
     A=${TXT_A%'</p><div '*}
+    # hint (number of words)
+    H=$(echo $A | wc -w)
 
     print -P "Riddle: %F{1}${R}??%f"
-    echo -n "Enter 'A/a' to see the Answer or enter your Answer: "
+    echo -n "Enter 'A/h' to see the Answer/Hint or enter your Answer: "
     read input
     echo "You entered: $input"
 
-    if [ $input = "A" ] || [ $input = "a" ]; then
-        print -P "And the answer is: %F{2}${A}%f"
-    else
-        echo -n "Do you want to see the answer now?(y/n): "
-        read user_ans
-        if [ $user_ans = "y" ]; then
+    case $input in
+        [Aa])
             print -P "And the answer is: %F{2}${A}%f"
-        elif [ $user_ans = "n" ]; then
-            echo -n "Take your time and at the end type something to see the answer..."
-            read something
-            print -P "And the answer is: %F{2}${A}%f"
-        else
-            print -P "And the answer is: %F{2}${A}%f"
-        fi
-    fi
+        ;;
+        [Hh])
+            echo -n "Hint: The answer has $H words in it...\n"
+            echo -n "Do you want to see the answer now?(Y/n): "
+            read user_ans
+            if [ $user_ans = "y" ] || [ $user_ans = "Y" ]; then
+                print -P "And the answer is: %F{2}${A}%f"
+            elif [ $user_ans = "n" ] || [ $user_ans = "N" ]; then
+                echo -n "Take your time and at the end type something to see the answer..."
+                read something
+                print -P "And the answer is: %F{2}${A}%f"
+            else
+                print -P "And the answer is: %F{2}${A}%f"
+            fi
+        ;;
+    esac
 }
