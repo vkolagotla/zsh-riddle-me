ab3abf9114446b553e7323f12ce39f9aed5ab49d
vkolagotla <venkata.kolagotla@gmail.com>
2021-04-14 22:07:41 +0200

    Update script version to v0.1.6

--- zsh-riddle-me.plugin.zsh
+++ zsh-riddle-me.plugin.zsh
@@ -6,7 +6,7 @@
 # Last Updated: 14-04-2021                                         	   //   \ \
 # Requires    : zsh, oh-my-zsh                                        (|     | )
 # Category    : zsh plugin                                       	 /'\_   _/`\\
-# Version     : v0.1.0                                               \___)=(___//
+# Version     : v0.1.6                                               \___)=(___//
 ##################################################################################
 # Description : shows some random riddles
 # Usage       : copy repo to zsh custom plugins dir
