889466401502966af6b4b71b2ccb7d1de2e9a0ef
vkolagotla <venkata.kolagotla@gmail.com>
2021-07-17 16:57:17 +0200

    Update file header

--- zsh-riddle-me.plugin.zsh
+++ zsh-riddle-me.plugin.zsh
@@ -1,31 +1,32 @@
 #!/bin/zsh
 ##################################################################################
-# Shellscript : zsh-riddle-me.plugin.zsh         					     .--.
-# Author      : Venkata Kolagotla <venkata.kolagotla@gmail.com>         |ö_ö |
-# Created     : 10-06-2020                                              |\ü/ |
-# Last Updated: 14-04-2021                                         	   //   \ \
-# Requires    : zsh, oh-my-zsh                                        (|     | )
-# Category    : zsh plugin                                       	 /'\_   _/`\\
-# Version     : v0.1.6                                               \___)=(___//
+# Shellscript : zsh-riddle-me.plugin.zsh                                 .--.    #
+# Author      : Venkata Kolagotla <venkata.kolagotla@gmail.com>         |ö_ö |   #
+# Created     : 10-06-2020                                              |\ü/ |   #
+# Last Updated: 17-07-2021                                             //   \ \  #
+# Requires    : zsh, oh-my-zsh, curl                                  (|     | ) #
+# Category    : zsh plugin                                           /'\_   _/`\\#
+# Version     : v0.1.7                                               \___)=(___//#
 ##################################################################################
-# Description : shows some random riddles
-# Usage       : copy repo to zsh custom plugins dir
+# Description : print random riddles
+# Usage       : riddle-me
 ##################################################################################
 
+# custom zsh plugin for random riddles
 function riddle-me {
 
     TXT=$(curl -s --connect-timeout 2 "https://goodriddlesnow.com/riddles/random" | iconv -c -f ISO-8859-1 -t UTF-8 | grep "<p><strong>")
-    
+
     # formatted riddle
     TXT_R=${TXT#*'<p><strong>Question: </strong>'}
     R=${TXT_R%'><ul class="inline-list print-hide">'*}
     R=$(echo "$R" | sed -e 's/<[^>]*>//g' | sed -e 's|?</div||g' | sed -e 's|</div||g')
-    
+
     # formatted answer
     TXT_A=${TXT_R#*'<p><strong>Answer: </strong>'}
     A=${TXT_A%'</p><div '*}
     A=$(echo "$A" | sed -e 's/<[^>]*>//g')
-    
+
     # hint (number of words)
     H=$(echo $A | wc -w)
 
@@ -40,9 +41,9 @@ function riddle-me {
         ;;
         [Hh])
             if [[ $H -eq 2 ]] && [[ $A =~ ^"A"||"An"||"The" ]]; then
-                echo -n "Hint: The answer has $H words in it and one of them is probably an article (A, An, The)...\n"
+                echo -n "\033[33mHint:\033[0m The answer has $H words in it and one of them is probably an article (A, An, The)...\n"
             else
-                echo -n "Hint: The answer has $H words in it... \n"
+                echo -n "\033[33mHint:\033[0m The answer has $H words in it... \n"
             fi
             echo -n "Do you want to see the answer now?(Y/n): "
             read user_ans
